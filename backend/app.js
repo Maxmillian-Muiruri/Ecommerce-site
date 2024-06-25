d6cb68a5acf4627df3e24f2b2e1a02e0e0657791
Maxmillian Muiruri <maxmillianmuiruri@gmail.com>
2024-06-25 13:29:59 +0300

    modify to handle errors

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 
 const app = express();
-
+const errorMiddleware = require("./middlewares/errors");
 app.use(express.json());
 
 // Import all routes
 const products = require("./routes/product");
 
 app.use("/api/v1/products", products);
+
+// midddeware to handle errors
+app.use(errorMiddleware);
 
 module.exports = app;
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,15 +1,18 @@
 const Product = require("../models/product");
 
+const ErrorHandler = require("../utils/errrorHandler");
+const catchAsyncErrors = require("../middlewares/catchSynchErrors");
+
 // Create new product   =>   /api/v1/admin/product/new
 
-exports.newProduct = async (req, res, next) => {
+exports.newProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
 
   res.status(201).json({
     success: true,
     product,
   });
-};
+});
 // Get all products   =>   /api/v1/products
 exports.getProducts = async (req, res, next) => {
   const products = await Product.find();
@@ -26,9 +29,13 @@ exports.getSingleProduct = async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
-    return res.status(404).json({ message: `Product not found` });
+    return next(new ErrorHandler("product not found", 404));
+    // res.status(404).json({ message: `Product not found` });
   }
-  res.status(200).json({ message: `success`, product });
+  res.status(200).json({
+    message: `success`,
+    product,
+  });
 };
 
 // UPDATE PRODUCT
