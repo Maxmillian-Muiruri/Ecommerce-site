e6d7fcda3ba2f24bf664d322cbbb324bf28c3d7b
Maxmillian Muiruri <maxmillianmuiruri@gmail.com>
2024-06-28 19:55:16 +0300

    send recovery email

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,8 +5,9 @@ const APIFeatures = require("../utils/apiFeatures");
 const catchAsyncErrors = require("../middlewares/catchSynchErrors");
 
 // Create new product   =>   /api/v1/admin/product/new
-
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
+  req.body.user = req.user.id;
+
   const product = await Product.create(req.body);
 
   res.status(201).json({
--- backend/models/product.js
+++ backend/models/product.js
@@ -95,6 +95,11 @@ const productSchema = new mongoose.Schema({
     ref: "User",
     required: true,
   },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
