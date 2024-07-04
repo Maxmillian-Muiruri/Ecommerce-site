5a4a428727ffe64d884a04c62b3baa0c11849ef4
Maxmillian Muiruri <maxmillianmuiruri@gmail.com>
2024-07-04 05:48:36 +0300

    adding order resources

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,11 @@ app.use(cookieParser());
 // Import all routes
 const products = require("./routes/product");
 const auth = require("./routes/auth");
+const order = require("./models/order");
 
 app.use("/api/v1/products", products);
 app.use("/api/v1", auth);
+app.use("/api/v1", order);
 
 // midddeware to handle errors
 app.use(errorMiddleware);
